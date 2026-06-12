370c63e3c2eff9cc3df55a157b35f9b328a1b395
Manoj Dubey <manojdubey27oct2007@gmail.com>
2026-06-13 02:10:07 +0530

    Fix: Set base path for GitHub Pages and rebuild

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: "/myportfoli/",
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
